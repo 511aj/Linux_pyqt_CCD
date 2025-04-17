b1edf9c4288faf5cc6d281e493981cbab62c76c9
511wj <2303082168@qq.com>
2025-04-18 00:01:38 +0800

    测试界面，修改画图函数，修改窗口大小，曲线上下翻转一下

--- main/sensor_reader.py
+++ main/sensor_reader.py
@@ -16,8 +16,8 @@ else:
 
 注意：
  串口号需要根据实际情况修改。
- com3 对应 传感器板
- com7 对应 电机控制板
+ ser 对应 传感器板
+ ser2 对应 电机控制板
 
 """
 
@@ -25,8 +25,10 @@ import serial
 import time
 
 
+# 读取传感器数据,电机和传感器的都写好了，直接调用就返回传感器数据
+# todo 需要修改串口号,电机发送位置信息还要找一下
 def read_sensor_data():
-    ser = serial.Serial("COM3", 256000, timeout=1)  # 打开串口
+    ser = serial.Serial("COM4", 256000, timeout=1)  # 打开串口
     ser2 = serial.Serial("COM7", 115200, timeout=1)  # 打开另一个串口
 
     if not ser.isOpen() or not ser2.isOpen():
@@ -60,13 +62,13 @@ def read_sensor_data():
         input_data = input_data.rstrip(', ')  # 删除最后多余的逗号和空格
 
     # 电机运动指令 走到终点
-    stop_end = "\x5A\xa5\x01\x01\x03\x0f\xf9\x01\x0d"  #\x5A\xa5\x01\x01\x03  这几项和为  104
+    stop_end = "\x5A\xa5\x01\x01\x03\x0f\xf9\x01\x0d"  # \x5A\xa5\x01\x01\x03  这几项和为  104
     ser2.write(stop_end.encode('utf-8'))
 
     time.sleep(1)
 
     # 电机运动指令 回到起点
-    stop_start = "\x5A\xa5\x01\x01\x03\x0f\xf9\x00\x0c"  #\x5A\xa5\x01\x01\x03\x0f   和为  113
+    stop_start = "\x5A\xa5\x01\x01\x03\x0f\xf9\x00\x0c"  # \x5A\xa5\x01\x01\x03\x0f   和为  113
     ser2.write(stop_start.encode('utf-8'))
 
     ser.close()  # 关闭串口
